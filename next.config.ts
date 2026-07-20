300ec439a97566529a1b3f4ef2c3d76f42074f8e
Mariayumi <maritamay4@gmail.com>
2026-07-20 20:02:02 -0300

    feat: começo da config do build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        basePath: false,
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
